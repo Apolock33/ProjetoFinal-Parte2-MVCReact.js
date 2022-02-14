c4afe036b4780640a64043a3fda9e4793ba6eb1c
Apolock33 <carlosammgomes@gmail.com>
2022-02-14 04:04:23 -0300

    Comit Com Componentes

--- AdaptSports/ClientApp/src/components/api-authorization/LoginMenu.js
+++ AdaptSports/ClientApp/src/components/api-authorization/LoginMenu.js
@@ -59,7 +59,7 @@ export class LoginMenu extends Component {
     anonymousView(registerPath, loginPath) {
         return (<Fragment>
             <NavItem>
-                <NavLink tag={Link} className="text-dark" to={registerPath}>Register</NavLink>
+                <NavLink tag={Link} className="text-dark" to={registerPath}>Cadastro</NavLink>
             </NavItem>
             <NavItem>
                 <NavLink tag={Link} className="text-dark" to={loginPath}>Login</NavLink>
