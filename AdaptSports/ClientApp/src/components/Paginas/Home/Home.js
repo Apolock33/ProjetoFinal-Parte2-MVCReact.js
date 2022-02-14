ed3d5a3b7e1ad76e7a76706131773fcca5cc2762
Apolock33 <carlosammgomes@gmail.com>
2022-02-14 16:38:16 -0300

    Pagina Locais completa

--- AdaptSports/ClientApp/src/components/Paginas/Home/Home.js
+++ AdaptSports/ClientApp/src/components/Paginas/Home/Home.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import './Home.css';
 import Footer from '../../Footer'
 import Card from '../../Card/Card';
 import ImgCard1 from '../../Assets/Img/deficiente-visual.jpg'
-import ImgCard2 from '../../Assets/Img/deficiente-visual.jpg'
-import ImgCard3 from '../../Assets/Img/deficiente-visual.jpg'
 
 export class Home extends Component {
     static displayName = Home.name;
@@ -13,14 +10,18 @@ export class Home extends Component {
     render() {
         return (
             <div className='HomePage'>
+
                 <section className='TextoPaiHome'>
+
                     <div className='Titulo1Home'>
                         <h1>Viva a Diferenca!</h1>
                     </div>
+
                     <p className='Texto1Home'>
                         Através do nosso site você fica informado sobre tudo relacionado a prática esportiva para Pessoas Com Deficiência, até mesmo se desejar competir, você pode checar nossa pagina com locais avaliados e informações disponiveis para o seu tipo de deficiência, pode marcar visita nesses mesmos locais, ou, se quiser praticar em outros locais que não estejam na lista, pode simplesmente verificar informações voltadas para o esporte que deseja conhecer.
                     </p>
                 </section>
+
                 <section className='TextoMaeHome'>
                     <div className='Titulo1Home'>
                         <h1>Descubra Mais!</h1>
@@ -29,15 +30,26 @@ export class Home extends Component {
                         Aqui você tambem pode ficar informado das noticias mais atuais no mundo do esporte paraolimpico e adaptado. Visite nossa sessão de noticias para saber os mais recentes acontecimentos. Você também pode conferir nosso rodapé. Lá se encontram os horários de atendimento que nosso site possui, e clicando nos simbolos você pode acessar nossas redes sociais e saber mais sobre nossa iniciativa e melhorias que pretendemos fazer.
                     </p>
                 </section>
+
                 <section className='Noticias'>
                     <div className='Divisor'>
                         <h1>Noticias</h1>
                     </div>
-                    <Card
-                        image={ImgCard1}
-                    />
-                    
+                    <div className='Cards'>
+                        <Card
+                            image={ImgCard1}
+                        />
+
+                        <Card
+                            image={ImgCard1}
+                        />
+                        
+                        <Card
+                            image={ImgCard1}
+                        />
+                    </div>
                 </section>
+
                 <footer>
                     <Footer />
                 </footer>
