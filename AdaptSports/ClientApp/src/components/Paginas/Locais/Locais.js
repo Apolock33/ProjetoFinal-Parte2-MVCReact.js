d55f243d026a397183446a6f589b58d006033732
Apolock33 <carlosammgomes@gmail.com>
2022-02-14 22:05:57 -0300

    Comit Atual

--- AdaptSports/ClientApp/src/components/Paginas/Locais/Locais.js
+++ AdaptSports/ClientApp/src/components/Paginas/Locais/Locais.js
@@ -28,6 +28,7 @@ export default class Locais extends Component {
                 <div className='Cards'>
                     <Card
                         className='CardHome'
+                        titulo='ADD - Associacao desportiva para deficientes'
                         img={ImgCard1}
                     />
 
@@ -41,26 +42,11 @@ export default class Locais extends Component {
                         img={ImgCard3}
                     />
                 </div>
-                <div className='Cards'>
-                    <Card
-                        className='CardHome'
-                        img={ImgCard1}
-                    />
-
-                    <Card
-                        className='CardHome'
-                        img={ImgCard2}
-                    />
-
-                    <Card
-                        className='CardHome'
-                        img={ImgCard3}
-                    />
-                </div>
+                
                 <footer>
                     <Footer />
                 </footer>
-            </div >
+            </div>
         );
     }
 }
